fe08eaabffcfa1cca9978b01219b45c8be84cf15
Eleftherios Kartakis <t8190064@aueb.gr>
2020-11-23 12:41:33 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,37 +2,44 @@ import java.util.Scanner;
 
 public class Student {
 
-	private int id; //ID μαθητή
-	private String name; //όνομα μαθητή
-	private String status;
-	public static Student[] students = new Student[10000]; //Στατικός Πίνακας 10000 θέσεων για τα αντικείμενα Student
+	private int id; //Student AM 
+	private String name; //Student name
+	private String status; //Student status
+	private boolean cl_contact; //indicates if Student object is a close contact of a positive case
+	public static Student[] students = new Student[10000]; //Static array of 10000 Student objects
 	static int count = 0;
 	public static int total_neg = 0;
 	public static int total_pos = 0;
 
-	//Δημιουργία κατασκευαστή της κλάσης για τη δημιουργία των αντικειμένων Student
-	public Student(int id, String name, String status) {
+	//Creation of Student objects constractor
+	public Student(int id, String name, String status, boolean cl_contact) {
 		students[count] = this;
 		count++;
 		this.id = id;
 		this.name = name;
 		this.status = status;
-	} //Τέλος Κατασκευαστή
+		this.cl_contact=cl_contact;
+	} //End of constractor
 
-	//Δημιουργία μεθόδου get για την τιμή της μεταβλητής int ID
+	//Method get for int ID
 	public int getID() {
 		return id;
-	} //Τέλος Μεθόδου
+	} //End of method
 
-	//Δημιουργία μεθόδου get για την τιμή της μεταβλητής String name
+	//Method get for String name
 	public String getName() {
 		return name;
-	} //Τέλος Μεθόδου
+	} //End of method
 
-	//Δημιουργία μεθόδου get για την τιμή της μεταβλητής String status
+	//Method get for String status
 	public String getStatus() {
 		return status;
-	} //Τέλος Μεθόδου
+	} //End of method
+	
+	//Method get for boolean cl_contact
+	public boolean cl_contact() {
+		return cl_contact;
+	} //End of method
 
 	public void setStatus(String status) {
 		this.status=status;
@@ -46,188 +53,208 @@ public class Student {
 		this.id=id;
 	}
 
-	//Δημιουργία μεθόδου για συμπλήρωση αποτελέσματος από μαθητές που έχουν κάνει τεστ ανίχνευσης COVID19.
+	//Method for new entry of Student covid-19 test result by the secretariat
 	public static void newStudent() {
         String name;
 		Scanner input = new Scanner(System.in);
 		String result;
+		System.out.print("Enter the name of the student: ");
+		name=input.nextLine();
+			
+		do {
+			System.out.print("Enter the result of the test (P/N): ");
+			result=input.nextLine();
+		} while (result != "P" || result != "N");
+		if (result == "P") {
+			positive();
+		} 
+		else {
+			negative();
+		}
 
-			do {
-				System.out.print("Εισάγετε το αποτέλεσμα του φοιτητή (Θ/Α): ");
-				result=input.nextLine();
-			} while (result != "Θ" || result != "Α");
-			if (result == "Θ") {
-				positive();
-			} else {
-				System.out.println("Εισάγετε όνομα φοιτητή.");
-				name = input.nextLine();
-				negative();
-		    }
-
-
-	 }
+	}
 	
-	 //Δημιουργία κλάσης για αρνητικά τεστ
-	 public static void negative() {
-	 total_neg++;
-	 System.out.println("Stay safe and protect yourself");
+	//Method for negative tests
+	public static void negative() {
+		total_neg++;
      }
 	 
-	 //Δημιουργία κλάσης για θετικά τεστ
-	 public static void positive() {
-		 changeStatus("P");
-		 String answer;
-		 String answer_2;
-		 String connection;
-		 total_pos++;
-		 Scanner scan = new Scanner(System.in);
-		 System.out.println("Πρέπει να μείνετε σε καραντίνα για 14 μέρες." + "Μπορείτε να επιστρέψετε στο πανεπιστήμιο όταν τα συμπτώματα υποχωρήσουν.");
-		 System.out.println("Έχετε έρθει σε επαφή με μέλη της πανεπιστημιακής κοινότητας τις τελευταίες 48 ώρες;");
+	//Method for positive cases
+	public static void positive() {
+		changeStatus("P");
+		String answer;
+		String answer_2;
+		int num;
+		String connection;
+		total_pos++;
+		Scanner scan1 = new Scanner(System.in);
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("You need to stay in quarantine for 14 days. You may return back to the university after all the symptoms resolve");
+		
+		System.out.println("Have you come in close contact with other AUEB students within the last 14 days?");
 		 
-		 do {
-		 answer = scan.nextLine();
-	     } while(answer!="Ναι" || answer!="Οχι");
+		do {
+			answer = scan1.nextLine();
+	     	} while(answer!="Yes" || answer!="No");
 	    	 
-		 if (answer=="Ναι") {
-		 do {
-			 System.out.println("Γράψτε το όνομα της επαφής.");
-			 connection = scan.nextLine();
-             positive_contacts();
-             System.out.println("Υπάρχει κάποια άλλη επαφή;");
-             
-             do {
-             answer_2 = scan.nextLine();
-             } while(answer_2 != "Ναι" || answer != "Οχι");
-             
-	     } while (answer_2 != "Οχι");
-	     }
-
-    }
+		if (answer=="Yes") {
+			System.out.println("How many students have you come in close contact with?");
+			num=scan1.nextInt();
+			
+			for(int i=0; i<num; i++) {
+				System.out.println("Enter student id: ");
+				close_contacts();
+			}
+		}
+			
+		System.out.println("Have you come in close contact with any AUEB staff members within the last 14 days?");
+		 
+		do {
+			answer = scan2.nextLine();
+		} while(answer!="Yes" || answer!="No");
+	    	 
+		if (answer=="Yes") {
+			System.out.println("How many emploees have you come in close contact with?");
+			num=scan1.nextInt();
+			
+			for(int i=0; i<num; i++) {
+				System.out.println("Enter emploee name: ");
+				close_contacts();
+			}
+		}
+		
+	}
 	 
-     //Δημιουργία κλάσης για τις ενδοπανεπιστημιακές επαφές των επιβεβαιωμένων κρουσμάτων
-     public static void positive_contacts() {
-		changeStatus("/");
+	//Method for close contacts
+    public static void close_contacts() {
+    	
+    	boolean cl_contact = true;
 		Scanner dm = new Scanner(System.in);
 		int p = 0;
 		int n = 0;
 		String an1;
-		System.out.println("Έχετε εκδηλώσει κάποιο από τα παρακάτω συμπτώματα");
-		System.out.println("Πυρετός:");
+		System.out.println("Have you manifested any of the symptoms mentioned below?");
+		
+		System.out.print("Fever: ");
 		do {
-		an1 = dm.nextLine();
-	    } while (an1 != "Ναι" || an1 != "Οχι");
-	    if (an1 == "Ναι") {
+			an1 = dm.nextLine();
+	   	} while (an1 != "Yes" || an1 != "No");
+	    if (an1 == "Yes") {
 			p = p + 1;
 		}
-		System.out.println("Fever:");
+		
+		System.out.print("Cough: ");
+	    	do {
+			an1 = dm.nextLine();
+	    	} while (an1 != "Yes" || an1 != "No");
+	    	if (an1 == "Yes") {
+			p = p + 1;
+		}
+	    	
+		System.out.print("Dyspnea: ");
 		do {
-		an1 = dm.nextLine();
-	    } while (an1 != "Ναι" || an1 != "Οχι");
-	    if (an1 == "Ναι") {
+			an1 = dm.nextLine();
+	    	} while (an1 != "Yes" || an1 != "No");
+		if (an1 == "Yes") {
 			p = p + 1;
 		}
-		System.out.println("Βήχας:");
-	    do {
-		an1 = dm.nextLine();
-	    } while (an1 != "Ναι" || an1 != "Οχι");
-	    if (an1 == "Ναι") {
-			p = p + 1;
-		}
-		System.out.println("Δύσπνοια:");
+		
+		System.out.print("Anosmia: ");
 		do {
-		an1 = dm.nextLine();
-	    } while (an1 != "Ναι" || an1 != "Οχι");
-		if (an1 == "Ναι") {
+			an1 = dm.nextLine();
+	    	} while (an1 != "Yes" || an1 != "No");
+		if (an1 == "Yes") {
 			p = p + 1;
 		}
-		System.out.println("Ανοσμία:");
+		
+		System.out.println("Ageusia: ");
 		do {
-		an1 = dm.nextLine();
-	    } while (an1 != "Ναι" || an1 != "Οχι");
-		if (an1 == "Ναι") {
+			an1 = dm.nextLine();
+	    } while (an1 != "Yes" || an1 != "No");
+		if (an1 == "Yes") {
 			p = p + 1;
 		}
-		System.out.println("Αγευσία:");
-		do {
-		an1 = dm.nextLine();
-	    } while (an1 != "Ναι" || an1 != "Οχι");
-		if (an1 == "Ναι") {
-			p = p + 1;
-		}
+		
 		if (p > 1) {
-			System.out.println("Πρέπει να κάνετε τεστ ανίχνευσης COVID19 και να μείνετε σε καραντίνα για τις επόμενες 14 μέρες ανεξάρτητα από το αποτέλεσμα.");
-		} else {
-		    System.out.println("Πονοκέφαλος:");
-		    do {
+			System.out.println("You need to do the covid-19 test, as well as stay in quarantine for 14 days");
+		} 
+		else {
+			System.out.print("Headaque: ");
+		    	do {
+		        	an1 = dm.nextLine();
+	        	} while (an1 != "Yes" || an1 != "No");
+		    	if (an1 == "Yes") {
+		    		n = n + 1;
+		    	}
+		    
+		    System.out.print("Shiver: ");
+		   	do {
 		        an1 = dm.nextLine();
-	        } while (an1 != "Ναι" || an1 != "Οχι");
-		    if (an1 == "Ναι") {
-			    n = n + 1;
+		    } while (an1 != "Yes" || an1 != "No");
+		    if (an1 == "Yes") {
+			   	n = n + 1;
 		    }
-		    System.out.println("Ρίγος:");
+		    
+		    System.out.print("Myalgias: ");
 		    do {
-		        an1 = dm.nextLine();
-		    } while (an1 != "Ναι" || an1 != "Οχι");
-		    if (an1 == "Ναι") {
-			    n = n + 1;
+		       	an1 = dm.nextLine();
+		    } while (an1 != "Yes" || an1 != "No");
+		    if (an1 == "Yes") {
+			   	n = n + 1;
 		    }
-		    System.out.println("Μυαλγίες:");
+		    
+		    System.out.print("Queasiness: ");
 		    do {
-		        an1 = dm.nextLine();
-		    } while (an1 != "Ναι" || an1 != "Οχι");
-		    if (an1 == "Ναι") {
-			    n = n + 1;
+		       	an1 = dm.nextLine();
+		    } while (an1 != "Yes" || an1 != "No");
+		    if (an1 == "Yes") {
+		       	n = n + 1;
 		    }
-		    System.out.println("Ναυτίες:");
+		    
+		    System.out.print("Diarrhea: ");
 		    do {
-		        an1 = dm.nextLine();
-		    } while (an1 != "Ναι" || an1 != "Οχι");
-		    if (an1 == "Ναι") {
-		        n = n + 1;
-		    }
-		    System.out.println("Διάρροια:");
-		    do {
-		        an1 = dm.nextLine();
-		    } while (an1 != "Ναι" | an1 != "Οχι");
-		    if (an1 == "Ναι") {
-			    n = n + 1;
+		       	an1 = dm.nextLine();
+		    } while (an1 != "Yes" | an1 != "No");
+		    if (an1 == "Yes") {
+			   n = n + 1;
 		    }
 		    if (n > 1) {
-				System.out.println("Πρέπει να μείνετε σε καραντίνα για τις επόμενες 10 μέρες. Εάν τα συμπτώματα δεν υποχωρήσουν πρέπει να κάνετε τεστ ανίχνευσης COVID19.");
+				System.out.println("You need to stay in quarantine for at least 10 days and, in case the symptoms do not resolve, you should then be tested for covid-19.");
 			}
-	    }
-	    if (p <=1 || n<=1) {
-			System.out.println("Εφόσον έχετε έρθει σε επαφή με επιβεβαιωμένο κρούσμα, πρέπει να μείνετε σε καραντίνα για τις επόμενες 7 μέρες. Εάν παρουσιάσετε κάποιο από τα προαναφερθέντα συμπτώματα πρέπει να κάνετε test ανίχνευσης COVID19.");
-		}
-
-    }
-     
-    //Δημιουργία κλάσης για την αλλαγή του status ασθένειας φοιτητή
-    public static void changeStatus(String status) {
-	    Scanner input = new Scanner(System.in);
-	    int id2;
-	    boolean found_sc=false;
-	    int con_found_sc = 0;
-	    do {
-			System.out.print("Πληκτρολογήστε ΑΜ φοιτητή. ");
-		    id2=input.nextInt();
-		} while (id2<1000000 || id2>9999999);
-
-	        for (int i=0; i<students.length; i++) {
-	            if (students[i].getID()==id2) {
-	                found_sc=true;
-	                con_found_sc=i;
-	                break;
-	            }
+	    	 
+		    if (p <=1 || n<=1) {
+		    	System.out.println("Since you have come in contact with a positive case, you should stay safe and, if you manifest any of the symptoms mentioned above, you may do the covid-19 test");
 		    }
 
-	if(found_sc == true) {
-	    students[con_found_sc].setStatus(status);
+    	}
+		
+    }
+     
+    //Method for status change
+	public static void changeStatus(String status) {
+		
+		Scanner input = new Scanner(System.in);
+	    int id2;
+	   	boolean found_sc=false;
+	    int con_found_sc = 0;
+	    
+	    do {
+			System.out.print("Enter student id: ");
+		    id2=input.nextInt();
+		} while (id2<8100000 || id2>8199999);
 
+	    for (int i=0; i<students.length; i++) {
+	    	if (students[i].getID()==id2) {
+	    		found_sc=true;
+	    		con_found_sc=i;
+	            break;
+	        }
+		}
+
+		if(found_sc == true) {
+	   		students[con_found_sc].setStatus(status);
+		}
+		
 	}
-
-
-
-}
 
 }
