581bc714ed0ae5a40629202ce9ccbeac6d45f9a7
ioannapaap <72801684+ioannapaap@users.noreply.github.com>
2020-11-26 20:34:08 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,48 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-
-		Scanner input1 = new Scanner(System.in);
-		int choice;
 		
-		displayMenu(); //ÅìöÜíéóç ìåíïý
-		choice=input1.nextInt();
-			
-		//¸ëåã÷ïò åãêõñüôçôáò åðéëïãÞò ÷ñÞóôç
-		while (choice<0 || choice>2) {
-			System.out.println("Ç åðéëïãÞ ðïõ åéóÜãáôå äåí åßíáé Ýãêõñç. Ðáñáêáëþ ðñïóðáèÞóôå ðÜëé.");
-			displayMenu(); //ÅìöÜíéóç ìåíïý
-			choice = input1.nextInt();
-		}
+		Generator.dataBase();
+		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		Staff.genStaff();
 		
-		switch(choice) {
-
-		case 0:
-			break; //¸îïäïò áðü ôï ìåíïý åðéëïãþí
-		case 1:
-			Student.newStudent(); 
-			break;
-		case 2:
-			Staff.newEmploee();
-			break;
-		
-		}
+		Scanner sc = new Scanner(System.in);
 
 	}
-	
-	public static void displayMenu() {
-		System.out.println("0. ¸îïäïò");
-		System.out.println("1. Êáôá÷þñçóç áðïôåëÝóìáôïò öïéôçôÞ");
-		System.out.println("2. Êáôá÷þñçóç áðïôåëÝóìáôïò åñãáæïìÝíïõ");
-		System.out.print("ÅéóÜãåôå åðéëïãÞ [0-2]: ");
-	}
 
-	Student s1 = new Student(8190064, "ÅëåõèÝñéïò ÊáñôÜêçò", "Á");
-	Student s2 = new Student(8190021, "ÁëÝîáíäñïò Ãåùñãïýëéáò", "Á");
-	Student s3 = new Student(8190036, "ÖñáôæÝóêïò ÄáöåñÝñáò", "Á");
-	Student s4 = new Student(8190044, "ÁíäñÝáò Äñüóïò", "Á");
-	Student s5 = new Student(8190106, "ÄçìÞôñéïò ÌáõñïåéäÞò", "Á");
-	Student s6 = new Student(8190132, "ÉùÜííá-Óùôçñßá Ðáðáäçìïýëç", "Á");
-	Student s7 = new Student(8190325, "Êùíóôáíôßíïò ÔæéáöÝôáò", "Á");
-	
 }
--- Staff.java
+++ Staff.java
@@ -1,236 +1,261 @@
-import java.util.Scanner;
+import java.util.Random;
 
+//Κλάση αυτή δημιουργεί 50 μέλη του προσωπικού του ΟΠΑ
 public class Staff {
-
-	private String name; //Emploee name
-	private String status; //Emploee status
-	private boolean cl_contact; //indicates if Staff object is a close contact of a positive case
-	public static Staff[] emploees = new Staff[10000]; //Static array of 10000 Staff objects
-	static int count = 0;
-	public static int total_neg = 0;
-	public static int total_pos = 0;
-
-	//Creation of Staff objects constractor
-	public Staff(String name, String status, boolean cl_contact) {
-		emploees[count] = this;
-		count++;
-		this.name = name;
-		this.status = status;
-		this.cl_contact=cl_contact;
-	} //End of constractor
-
-	//Method get for String name
-	public String getName() {
-		return name;
-	} //End of method
-
-	//Method get for String status
-	public String getStatus() {
-		return status;
-	} //End of method
 	
-	//Method get for boolean cl_contact
-	public boolean cl_contact() {
-		return cl_contact;
-	} //End of method
-
-	public void setStatus(String status) {
-		this.status=status;
-	}
-
-	public void setName(String name) {
-		this.name=name;
-	}
-
-	public void setID(int id) {
-		this.id=id;
-	}
-
-	//Method for new entry of Staff covid-19 test result by the secretariat
-	public static void newStudent() {
-        String name;
-		Scanner input = new Scanner(System.in);
-		String result;
-		System.out.print("Enter the name of the emploee: ");
-		name=input.nextLine();
-			
-		do {
-			System.out.print("Enter the result of the test (P/N): ");
-			result=input.nextLine();
-		} while (result != "P" || result != "N");
-		if (result == "P") {
-			positive();
-		} 
-		else {
-			negative();
-		}
-
-	}
+	/* Η μέθοδος εκτυπώνει 50 μέλη του προσωπικού που εργάζεται στο ΟΠΑ
+	   σε μορφή δημιουργία αντικειμένων (Staff counter = new Satff(,name,surname,status,closecontact)
+	   προκυμένου με copy της εκτυπωση της μεθόδου και paste στο κύριο πρόγραμμα στη κλάση δημιουργίας των Staff να
+	   δημιουργηθούν αυτόματα οι Staff ετσι ώστε να έχουμε τα ίδια στοιχεία για κάθε εκτέλεση του προγράμματος 
+	*/
+	public static void genStaff() {
 	
-	//Method for negative tests
-	public static void negative() {
-		total_neg++;
-     }
-	 
-	//Method for positive cases
-	public static void positive() {
-		changeStatus("P");
-		String answer;
-		String answer_2;
-		int num;
-		String connection;
-		total_pos++;
-		Scanner scan1 = new Scanner(System.in);
-		Scanner scan2 = new Scanner(System.in);
-		System.out.println("You need to stay in quarantine for 14 days. You may return back to the university after all the symptoms resolve");
+	
+	int counter = 1;
+	String namesmen[] = new String [50];
+	String surnamemen[] = new String [60];
+	String nameswomen[] = new String [50];
+	String surnamewomen[] = new String [60];
+	Random rand = new Random();
+	
+	namesmen[0] = "Νικόλαος";
+	namesmen[1] = "Γεώργιος";
+	namesmen[2] = "Δημήτριος";
+	namesmen[3] = "Κωνσταντίνος";
+	namesmen[4] = "Μάριος";
+	namesmen[5] = "Παρασκευάς";
+	namesmen[6] = "Βασίλης";
+	namesmen[7] = "Μιχαήλ";
+	namesmen[8] = "Ιωάννης";
+	namesmen[9] = "Θεόδωρος";
+	namesmen[10] = "Χρήστος";
+	namesmen[11] = "Σπυρίδων";
+	namesmen[12] = "Παναγιώτης";
+	namesmen[13] = "Αντώνιος";
+	namesmen[14] = "Βασίλειος";
+	namesmen[15] = "Εμμανουήλ";
+	namesmen[16] = "Αθανάσιος";
+	namesmen[17] = "Εμμανουήλ";
+	namesmen[18] = "Ευάγγελος";
+	namesmen[19] = "Φρατζέσκος";
+	namesmen[20] = "Ηλίας";
+	namesmen[21] = "Αλέξανδρος";
+	namesmen[22] = "Άγγελος";
+	namesmen[22] = "Απόστολος";
+	namesmen[23] = "Χαράλαμπρος";
+	namesmen[24] = "Σταύρος";
+	namesmen[25] = "Πέτρος";
+	namesmen[26] = "Στυλιανός";
+	namesmen[27] = "Σωτήριος";
+	namesmen[28] = "Ανδρέας";
+	namesmen[29] = "Θωμάς";
+	namesmen[30] = "Στέφανος";
+	namesmen[31] = "Ελευθέριος";
+	namesmen[32] = "Παύλος";
+	namesmen[33] = "Κυριάκος";
+	namesmen[34] = "Λεωνίδας";
+	namesmen[35] = "Αχιλλέας";
+	namesmen[36] = "Σωκράτης";
+	namesmen[37] = "Ορέστης";
+	namesmen[38] = "Ανάργυρος";
+	namesmen[39] = "Γρηγόριος";
+	namesmen[40] = "Λάζαρος";
+	namesmen[41] = "Γεράσιμος";
+	namesmen[42] = "Δήμος";
+	namesmen[43] = "Αργύριος";
+	namesmen[44] = "Νεκτάριος";
+	namesmen[45] = "Χριστόφορος";
+	namesmen[46] = "Φίλιππός";
+	namesmen[47] = "Θεοχάρης";
+	namesmen[48] = "Ζαχαρίας";
+	namesmen[49] = "Ανέστης";
+	
+	surnamemen[0] = "Αλεξίου";
+	surnamemen[1] = "Δημητρακόπουλος";
+	surnamemen[2] = "Διαμαντόπουλος";
+	surnamemen[3] = "Αθανασίου";
+	surnamemen[4] = "Ελευθερόπουλος";
+	surnamemen[5] = "Αναγνωστάκης";
+	surnamemen[6] = "Ευθιμιάδης";
+	surnamemen[7] = "Ζέρβας";
+	surnamemen[8] = "Ανδρέου";
+	surnamemen[9] = "Ζαφειρόπουλος";
+	surnamemen[10] = "Αντωνόπουλος";
+	surnamemen[11] = "Ηλιόπουλος";
+	surnamemen[12] = "Αναγνώστου";
+	surnamemen[13] = "Θεοδορίδης";
+	surnamemen[14] = "Αποστολάκης";
+	surnamemen[15] = "Θεοδοσίου";
+	surnamemen[16] = "Ιωαννίδης";
+	surnamemen[17] = "Βαμβακάς";
+	surnamemen[18] = "Ιωάννου";
+	surnamemen[19] = "Βασιλείου";
+	surnamemen[20] = "Καλογεράς";
+	surnamemen[21] = "Βασιλειάδης";
+	surnamemen[22] = "Καλογιάννης";
+	surnamemen[23] = "Βασιλόπουλος";
+	surnamemen[24] = "Καραμάνος";
+	surnamemen[25] = "Βογιατζής";
+	surnamemen[26] = "Κατράκης";
+	surnamemen[27] = "Βιτάλης";
+	surnamemen[28] = "Κεχαγιάς";
+	surnamemen[29] = "Γαλάνης";
+	surnamemen[30] = "Γαλανόπουλος";
+	surnamemen[31] = "Κεφαλογιάννης";
+	surnamemen[32] = "Κοκκινάκης";
+	surnamemen[33] = "Γεωργίου";
+	surnamemen[34] = "Γεωργιάδης";
+	surnamemen[35] = "Κοτζιάς";
+	surnamemen[36] = "Γιαννόπουλος";
+	surnamemen[37] = "Γιαννακόπουλος";
+	surnamemen[38] = "Κουβέλης";
+	surnamemen[39] = "Γιάνναρης";
+	surnamemen[40] = "Κουντουριώτης";
+	surnamemen[41] = "Γιαννούλης";
+	surnamemen[42] = "Κυριακίδης";
+	surnamemen[43] = "Γούναρης";
+	surnamemen[44] = "Κωσταντίνου";
+	surnamemen[45] = "Κωσταντινόπουλος";
+	surnamemen[46] = "Δασκαλόπουλος";
+	surnamemen[47] = "Δασκαλάκης";
+	surnamemen[48] = "Δρόσος";
+	surnamemen[49] = "Κατσιγιάννης";
+	surnamemen[50] = "Ανδρεάδης";
+	surnamemen[51] = "Γεωργούλιας";
+	surnamemen[52] = "Μαυροειδής";
+	surnamemen[53] = "Τζιαφέτας";
+	surnamemen[54] = "Δαφερέρας";
+	surnamemen[55] = "Δρόσος";
+	surnamemen[56] = "Καρτάκης";
+	surnamemen[57] = "Αργυρός";
+	surnamemen[58] = "Ασλανίδης";
+	surnamemen[59] = "Παπαδόπουλος";
+	
+	nameswomen[0] = "Μαρία";
+	nameswomen[1] = "Ελένη";
+	nameswomen[2] = "Αικατερίνη";
+	nameswomen[3] = "Βασιλική";
+	nameswomen[4] = "Γεωργία";
+	nameswomen[5] = "Σοφία";
+	nameswomen[6] = "Αναστασία";
+	nameswomen[7] = "Ευαγγελία";
+	nameswomen[8] = "Ιωάννα";
+	nameswomen[9] = "Δήμητρα";
+	nameswomen[10] = "Ειρήνη";
+	nameswomen[11] = "Παναγιώτα";
+	nameswomen[12] = "Χριστίνα";
+	nameswomen[13] = "Κωνσταντίνα";
+	nameswomen[14] = "Αννα";
+	nameswomen[15] = "Αγγελική";
+	nameswomen[16] = "Δέσποινα";
+	nameswomen[17] = "Παρασκευή";
+	nameswomen[18] = "Φωτεινή";
+	nameswomen[19] = "Αλεξάνδρα";
+	nameswomen[20] = "Κυριακή";
+	nameswomen[21] = "Θεοδώρα";
+	nameswomen[22] = "Σταυρούλα";
+	nameswomen[23] = "Χρυσούλα";
+	nameswomen[24] = "Καλλιόπη";
+	nameswomen[25] = "Αθανασία";
+	nameswomen[26] = "Ελευθερία";
+	nameswomen[27] = "Αθηνά";
+	nameswomen[28] = "Στυλιανή";
+	nameswomen[29] = "Ολγα";
+	nameswomen[30] = "Ζωή";
+	nameswomen[31] = "Ελισάβετ";
+	nameswomen[32] = "Ευγενία";
+	nameswomen[33] = "Μαρίνα";
+	nameswomen[34] = "Αντωνία";
+	nameswomen[35] = "Ουρανία";
+	nameswomen[36] = "Μαγδαληνη";
+	nameswomen[37] = "Χρυσάνθη";
+	nameswomen[38] = "Αφροδίτη";
+	nameswomen[39] = "Νικολέτα";
+	nameswomen[40] = "Αμαλία";
+	nameswomen[41] = "Ασπασία";
+	nameswomen[42] = "Μάρθα";
+	nameswomen[43] = "Ευδοκία";
+	nameswomen[44] = "Αντιγόνη";
+	nameswomen[45] = "Αρετή";
+	nameswomen[46] = "Ελπίδα";
+	nameswomen[47] = "Τριανταφυλλιά";
+	nameswomen[48] = "Δάφνη";
+	nameswomen[49] = "Άρτεμις";
+	
+	surnamewomen[0] = "Αβαγιανέλλη";
+	surnamewomen[1] = "Βαγοπούλου";
+	surnamewomen[2] = "Αγαδάκη";
+	surnamewomen[3] = "Αβραμάκου";
+	surnamewomen[4] = "Βαγκαλιώτου";
+	surnamewomen[5] = "Βαζακίδη";
+	surnamewomen[6] = "Βαρθιωτάκη";
+	surnamewomen[7] = "Γαβαλάκη";
+	surnamewomen[8] = "Γαζηδέλλη";
+	surnamewomen[9] = "Γαβρεηλίδου";
+	surnamewomen[10] = "Γαλαζοπούλου";
+	surnamewomen[11] = "Παπαδημούλη";
+	surnamewomen[12] = "Δαβάκη";
+	surnamewomen[13] = "Δαδάκου";
+	surnamewomen[14] = "Δαβλιάκου";
+	surnamewomen[15] = "Δαλδάρη";
+	surnamewomen[16] = "Ελβατζή";
+	surnamewomen[17] = "Ελευθεριάδη";
+	surnamewomen[18] = "Εδιπίδη";
+	surnamewomen[19] = "Κοντιζά";
+	surnamewomen[20] = "Εκμετζίδου";
+	surnamewomen[21] = "Ζαβαλιάδου";
+	surnamewomen[22] = "Ζαγκίδου";
+	surnamewomen[23] = "Ζακουμίδη";
+	surnamewomen[24] = "Ηγουμενάκη";
+	surnamewomen[25] = "Ηλιαδάκη";
+	surnamewomen[26] = "Ηλιοπούλου";
+	surnamewomen[27] = "Θαμνοπούλου";
+	surnamewomen[28] = "Θαναηλάκη";
+	surnamewomen[29] = "Θανιώτη";
+	surnamewomen[30] = "Ικαριώτου";
+	surnamewomen[31] = "Ιατράκη";
+	surnamewomen[32] = "Καβελάκη";
+	surnamewomen[33] = "Λαγοθμάκη";
+	surnamewomen[34] = "Λαγωγιάννη";
+	surnamewomen[35] = "Μαγγανιώτου";
+	surnamewomen[36] = "Μαγωγιάννη";
+	surnamewomen[37] = "Νανιοπούλου";
+	surnamewomen[38] = "Ναζαρίδη";
+	surnamewomen[39] = "Νασιώτη";
+	surnamewomen[40] = "Ξανθάκη";
+	surnamewomen[41] = "Ξενογιαννάκη";
+	surnamewomen[42] = "Παπαγεωργακοπούλου";
+	surnamewomen[43] = "Παντουβεράκη";
+	surnamewomen[44] = "Παπαγιαννάτου";
+	surnamewomen[45] = "Παπαγιαννούλη";
+	surnamewomen[46] = "Αβαγιανέλλη";
+	surnamewomen[47] = "Σακελλάρη";
+	surnamewomen[48] = "Σαμαρίνη";
+	surnamewomen[49] = "Σεϊνταρίδη";
+	surnamewomen[50] = "Σειραδάκη";
+	surnamewomen[51] = "Σιάκου";
+	surnamewomen[52] = "Σιββοπούλου";
+	surnamewomen[53] = "Ταβλαρίδη";
+	surnamewomen[54] = "Ταγανίδου";
+	surnamewomen[55] = "Χαλαμανίδου";
+	surnamewomen[56] = "Χανίδου";
+	surnamewomen[57] = "Θεολογίτη";
+	surnamewomen[58] = "Θανογιάννη";
+	surnamewomen[59] = "Θεοχάρη";
+	
+	
+	for(int i=1 ; i<= 25; i++) {
+		int randname = rand.nextInt(50);
+		int randsurname = rand.nextInt(60);
 		
-		System.out.println("Have you come in close contact with any AUEB students within the last 14 days?");
-		 
-		do {
-			answer = scan1.nextLine();
-	     	} while(answer!="Yes" || answer!="No");
-	    	 
-		if (answer=="Yes") {
-			System.out.println("How many students have you come in close contact with?");
-			num=scan1.nextInt();
-			
-			for(int i=0; i<num; i++) {
-				System.out.println("Enter student id: ");
-				close_contacts();
-			}
-		}
-			
-		System.out.println("Have you come in close contact with other AUEB staff members within the last 14 days?");
-		 
-		do {
-			answer = scan2.nextLine();
-		} while(answer!="Yes" || answer!="No");
-	    	 
-		if (answer=="Yes") {
-			System.out.println("How many emploees have you come in close contact with?");
-			num=scan1.nextInt();
-			
-			for(int i=0; i<num; i++) {
-				System.out.println("Enter emploee name: ");
-				close_contacts();
-			}
-		}
+	
+		System.out.println("INSERT INTO Staff (name,surname,status,closecontact)\nValues ('" + namesmen[randname] + "', '"+ surnamemen[randsurname] + "','n','false');" );
+		//counter++;
+		System.out.println("INSERT INTO Staff (name,surname,status,closecontact)\nValues ('" + nameswomen[randname] + "', '"+ surnamewomen[randsurname] + "','n','false');" );
+		//counter++;
+		
 		
 	}
-	 
-	//Method for close contacts
-    public static void close_contacts() {
-    	
-    	boolean cl_contact = true;
-		Scanner dm = new Scanner(System.in);
-		int p = 0;
-		int n = 0;
-		String an1;
-		System.out.println("Have you manifested any of the symptoms mentioned below?");
-		
-		System.out.print("Fever: ");
-		do {
-			an1 = dm.nextLine();
-	   	} while (an1 != "Yes" || an1 != "No");
-	    if (an1 == "Yes") {
-			p = p + 1;
-		}
-		
-		System.out.print("Cough: ");
-	    	do {
-			an1 = dm.nextLine();
-	    	} while (an1 != "Yes" || an1 != "No");
-	    	if (an1 == "Yes") {
-			p = p + 1;
-		}
-	    	
-		System.out.print("Dyspnea: ");
-		do {
-			an1 = dm.nextLine();
-	    	} while (an1 != "Yes" || an1 != "No");
-		if (an1 == "Yes") {
-			p = p + 1;
-		}
-		
-		System.out.print("Anosmia: ");
-		do {
-			an1 = dm.nextLine();
-	    	} while (an1 != "Yes" || an1 != "No");
-		if (an1 == "Yes") {
-			p = p + 1;
-		}
-		
-		System.out.println("Ageusia: ");
-		do {
-			an1 = dm.nextLine();
-	    } while (an1 != "Yes" || an1 != "No");
-		if (an1 == "Yes") {
-			p = p + 1;
-		}
-		
-		if (p > 1) {
-			System.out.println("You need to do the covid-19 test, as well as stay in quarantine for 14 days");
-		} 
-		else {
-			System.out.print("Headaque: ");
-		    	do {
-		        	an1 = dm.nextLine();
-	        	} while (an1 != "Yes" || an1 != "No");
-		    	if (an1 == "Yes") {
-		    		n = n + 1;
-		    	}
-		    
-		    System.out.print("Shiver: ");
-		   	do {
-		        an1 = dm.nextLine();
-		    } while (an1 != "Yes" || an1 != "No");
-		    if (an1 == "Yes") {
-			   	n = n + 1;
-		    }
-		    
-		    System.out.print("Myalgias: ");
-		    do {
-		       	an1 = dm.nextLine();
-		    } while (an1 != "Yes" || an1 != "No");
-		    if (an1 == "Yes") {
-			   	n = n + 1;
-		    }
-		    
-		    System.out.print("Queasiness: ");
-		    do {
-		       	an1 = dm.nextLine();
-		    } while (an1 != "Yes" || an1 != "No");
-		    if (an1 == "Yes") {
-		       	n = n + 1;
-		    }
-		    
-		    System.out.print("Diarrhea: ");
-		    do {
-		       	an1 = dm.nextLine();
-		    } while (an1 != "Yes" | an1 != "No");
-		    if (an1 == "Yes") {
-			   n = n + 1;
-		    }
-		    if (n > 1) {
-				System.out.println("You need to stay in quarantine for at least 10 days and, in case the symptoms do not resolve, you should then be tested for covid-19.");
-			}
-	    	 
-		    if (p <=1 || n<=1) {
-		    	System.out.println("Since you have come in contact with a positive case, you should stay safe and, if you manifest any of the symptoms mentioned above, you may do the covid-19 test");
-		    }
+	}
 
-    	}
-		
-    }
-     
-    //Method for status change
-	public static void changeStatus(String status) {
-		
-		Scanner input = new Scanner(System.in);
-	    String name;
-	   	System.out.print("Enter emploee's name: ");
-		name=input.nextLine();
-		setStatus(status);
-	}
-		
 }
