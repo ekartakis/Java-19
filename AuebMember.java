f8631263274311d77634f0860e2d8d421fca3f8b
Fratzeskos Dafereras <72812676+Fratzeskos@users.noreply.github.com>
2020-12-01 03:15:54 +0200

    Add files via upload

---
+++ AuebMember.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public abstract class AuebMember {
+	
+	public abstract void iWantMyStat(int id);
+	public abstract void iWantMyCloseCont(int id);
+	public abstract void iWantTheStatistics();
+	public abstract String giveMyStatus(int id);
+	public abstract String closeCont(int id);
+}
