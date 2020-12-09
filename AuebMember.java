b931ff52a0dd0c6752f782246762d8cb493dad34
ioannapaap <72801684+ioannapaap@users.noreply.github.com>
2020-12-09 17:05:32 +0200

    Add files via upload

--- AuebMember.java
+++ AuebMember.java
@@ -3,6 +3,8 @@ public abstract class AuebMember {
 	public abstract void iWantMyStat(int id);
 	public abstract void iWantMyCloseCont(int id);
 	public abstract void iWantTheStatistics();
+	public abstract void interactionWithUser();
+	public abstract int menu();
 	public abstract String giveMyStatus(int id);
 	public abstract String closeCont(int id);
 }
