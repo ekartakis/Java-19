45552cb9e55cf6a1956523015802077a68b7db8d
dmavroeidis01 <72823402+dmavroeidis01@users.noreply.github.com>
2020-12-10 10:22:45 +0200

    Add files via upload

---
+++ StudentNoCloseCon.java
@@ -0,0 +1,49 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class StudentNoCloseCon extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StudentNoCloseCon frame = new StudentNoCloseCon();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StudentNoCloseCon() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("You are not a close contact!!");
+		lblNewLabel.setBounds(12, 13, 420, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Stay healthy!");
+		lblNewLabel_1.setBounds(12, 38, 85, 16);
+		contentPane.add(lblNewLabel_1);
+	}
+
+}
