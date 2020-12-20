74eb8f304f09ea0a33e34cd6621db3deaf2d40a4
Kostas Tziafetas <72854285+KostasTziafetas@users.noreply.github.com>
2020-12-20 20:24:15 +0200

    Update UniversityAdministrationMenu.java

--- UniversityAdministrationMenu.java
+++ UniversityAdministrationMenu.java
@@ -1,6 +1,6 @@
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
+
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -8,9 +8,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.EventQueue;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+import javax.swing.SwingConstants;
+import java.awt.Color;
 
 public class UniversityAdministrationMenu extends JFrame {
 
@@ -44,44 +47,67 @@ public class UniversityAdministrationMenu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
 		JButton btnNewButton = new JButton("Add a new student case.");
-		btnNewButton.setBounds(286, 91, 173, 57);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Add a new staff case.");
-		btnNewButton_1.setBounds(286, 161, 173, 58);
-		contentPane.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("Show statistics.");
-		btnNewButton_2.setBounds(286, 232, 173, 57);
-		contentPane.add(btnNewButton_2);
-		
-		JLabel lblNewLabel = new JLabel("Options:");
-		lblNewLabel.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 20));
-		lblNewLabel.setBounds(342, 36, 59, 22);
-		contentPane.add(lblNewLabel);
-		
-		JButton btnExitButton;
-		btnExitButton = new JButton("Exit");
-		btnExitButton.addActionListener(new ActionListener() {
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
+				setVisible(false);
+				AddNewStCase addnewstcase = new AddNewStCase();
+				addnewstcase.setVisible(true);
 			}
 		});
-		btnExitButton.setFont(new Font("Tahoma", Font.BOLD, 24));
-		btnExitButton.setBounds(286, 299, 173, 44);
-		contentPane.add(btnExitButton);
-		
-		JButton btnNewButton_3 = new JButton("Back");
-		btnNewButton_3.addActionListener(new ActionListener() {
+		btnNewButton.setBounds(286, 91, 173, 57);
+		contentPane.add(btnNewButton);
+
+		JButton btnNewButton_1 = new JButton("Add a new staff case.");
+		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				AddNewStaffCase addnewstaffcase = new AddNewStaffCase();
+				addnewstaffcase.setVisible(true);
+			}
+		});
+		btnNewButton_1.setBounds(286, 161, 173, 58);
+		contentPane.add(btnNewButton_1);
+
+		JButton btnNewButton_2 = new JButton("Process statistics.");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btnNewButton_2.setBounds(286, 232, 173, 57);
+		contentPane.add(btnNewButton_2);
+
+		JLabel lblNewLabel = new JLabel("Options:");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tw Cen MT Condensed", Font.BOLD, 24));
+		lblNewLabel.setBounds(284, 25, 173, 55);
+		contentPane.add(lblNewLabel);
+
+		JButton btnBackButton;
+		btnBackButton = new JButton("Back");
+		btnBackButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 				setVisible(false);
 				MainMenu mainmenu = new MainMenu();
 				mainmenu.setVisible(true);
 			}
 		});
-		btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnNewButton_3.setBounds(12, 339, 97, 25);
-		contentPane.add(btnNewButton_3);
+		btnBackButton.setFont(new Font("Tahoma", Font.BOLD, 24));
+		btnBackButton.setBounds(10, 322, 144, 37);
+		contentPane.add(btnBackButton);
+		
+		JButton btnExitButton1 = new JButton("Exit");
+		btnExitButton1.setForeground(Color.RED);
+		btnExitButton1.setFont(new Font("Tahoma", Font.BOLD, 24));
+		btnExitButton1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+
+			}
+		});
+		btnExitButton1.setBounds(584, 322, 144, 37);
+		contentPane.add(btnExitButton1);
 	}
 }
