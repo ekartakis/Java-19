69a101dbe61e2b5b1c8106ca6b6985a277729d54
Fratzeskos Dafereras <72812676+Fratzeskos@users.noreply.github.com>
2020-12-23 12:32:49 +0200

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -7,6 +7,65 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Staff extends AuebMember {
+	
+	@Override
+	public String giveTheDateOfPos(int AFM) {
+		String result = "You inserted a wrong AFM";
+		String url = "jdbc:sqlite:Thefinaldb.db";
+		String quer = "SELECT AFM FROM Staff WHERE AFM = " + AFM;
+		String sql = "SELECT DatePos FROM Staff WHERE AFM = " + AFM;
+		;
+		try {Connection conn = DriverManager.getConnection(url);
+
+		Statement firststmt  = conn.createStatement();
+		ResultSet firstrs    = firststmt.executeQuery(quer);
+		Statement stmt  = conn.createStatement();
+		ResultSet rs    = stmt.executeQuery(sql);
+		if(firstrs.next() == true) {
+
+			result = rs.getString("DatePos");
+		} 
+		else {result = "\nYou inserted a wrong AFM";}
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return result;
+	}
+	
+	public String giveTheDateOfCloseCont(int AFM) {
+		String result = "You inserted a wrong AFM";
+		String url = "jdbc:sqlite:Thefinaldb.db";
+		String quer = "SELECT AFM FROM Staff WHERE AFM = " + AFM;
+		String sql = "SELECT DateClose FROM Staff WHERE AFM = " + AFM;
+		;
+		try {Connection conn = DriverManager.getConnection(url);
+
+		Statement firststmt  = conn.createStatement();
+		ResultSet firstrs    = firststmt.executeQuery(quer);
+		Statement stmt  = conn.createStatement();
+		ResultSet rs    = stmt.executeQuery(sql);
+		if(firstrs.next() == true) {
+
+			result = rs.getString("DateClose");
+		} 
+		else {result = "\nYou inserted a wrong AFM";}
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return result;
+	}
 
 	@Override
 	public void iWantMyStat(int id) {
