dcde217bc9fa89eae254dc62c5167a0aa2ad2f60
Kostas Tziafetas <72854285+KostasTziafetas@users.noreply.github.com>
2021-01-24 19:09:14 +0200

    Add files via upload

--- StudentNoSymptoms.java
+++ StudentNoSymptoms.java
@@ -15,74 +15,80 @@ import java.awt.Color;
 
 public class StudentNoSymptoms extends JFrame {
 
-	private JPanel contentPane;
+  private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					StudentNoSymptoms frame = new StudentNoSymptoms();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+  /**
+ * Launch the application.
+ */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          StudentNoSymptoms frame = new StudentNoSymptoms();
+          frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+          }
+      }
+      });
+  }
 
-	/**
-	 * Create the frame.
-	 */
-	public StudentNoSymptoms() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0,0,1280,730);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Stay in quarantine for 7 days from " + StudentCloseCon.dateclosecont);
-		lblNewLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 27));
-		lblNewLabel.setBounds(28, 38, 893, 47);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("and if you have any of the above symptoms do the covid-19 test!");
-		lblNewLabel_1.setFont(new Font("Bookman Old Style", Font.PLAIN, 27));
-		lblNewLabel_1.setBounds(28, 96, 919, 47);
-		contentPane.add(lblNewLabel_1);
-		
-		JButton btnBackButton = new JButton("Back");
-		btnBackButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-				CollStudentMenu csm = new CollStudentMenu();
-				csm.setVisible(true);
-			}
-		});
-		btnBackButton.setFont(new Font("Bookman Old Style", Font.BOLD, 35));
-		btnBackButton.setBounds(47, 529, 215, 71);
-		contentPane.add(btnBackButton);
-		
-		JLabel lblImage10 = new JLabel("");
-		Image img10 = new ImageIcon(this.getClass().getResource("/Pics/135464593_1077636602678989_8400056473973577538_n.png")).getImage();
-		lblImage10.setIcon(new ImageIcon(img10));
-		lblImage10.setBounds(981, 0, 285, 233);
-		contentPane.add(lblImage10);
-		
-		Image img4 = new ImageIcon(this.getClass().getResource("/Pics/left-arrow (1).png")).getImage();
-		btnBackButton.setIcon(new ImageIcon(img4));
-		btnBackButton.setFont(new Font("Tahoma", Font.BOLD, 24));
-		contentPane.add(btnBackButton);
-		
-		JLabel lblImage14 = new JLabel("");
-		Image img14 = new ImageIcon(this.getClass().getResource("/Pics/stay-home.png")).getImage();
-		lblImage14.setIcon(new ImageIcon(img14));
-		lblImage14.setBounds(560, 265, 128, 128);
-		contentPane.add(lblImage14);
-	}
+  /**
+ * Create the frame.
+ */
+  //In this case the student is a close contact but has no symptoms.
+  public StudentNoSymptoms() {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(0, 0, 1280, 730); 
+    contentPane = new JPanel();
+    contentPane.setBackground(Color.WHITE);
+    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    setContentPane(contentPane);
+    contentPane.setLayout(null);
+    //Creating and displaying all the labels to write the text.
+    JLabel lblNewLabel = new JLabel("Stay in quarantine for "
+        + "7 days from " + StudentCloseCon.dateclosecont);
+    lblNewLabel.setFont(new Font("Bookman Old Style", Font.PLAIN, 27));
+    lblNewLabel.setBounds(28, 38, 893, 47);
+    contentPane.add(lblNewLabel);
 
+    JLabel lblNewLabel1 = new JLabel("and if you have any of the above"
+        + " symptoms do the covid-19 test!");
+    lblNewLabel1.setFont(new Font("Bookman Old Style", Font.PLAIN, 27));
+    lblNewLabel1.setBounds(28, 96, 919, 47);
+    contentPane.add(lblNewLabel1);
+    //Creating and displaying the back button.
+    //The back button returns to the CollegeStudentMenu class.
+    JButton btnBackButton = new JButton("Back");
+    btnBackButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        setVisible(false);
+        CollStudentMenu csm = new CollStudentMenu();
+        csm.setVisible(true);
+      }
+    });
+    btnBackButton.setFont(new Font("Bookman Old Style", Font.BOLD, 35));
+    btnBackButton.setBounds(47, 529, 215, 71);
+    contentPane.add(btnBackButton);
+    //Creating and displaying all the images in labels and buttons.
+    JLabel lblImage10 = new JLabel("");
+    Image img10 = new ImageIcon(this.getClass().getResource("/Pics/135464593_1"
+        + "077636602678989_8400056473973577538_n.png")).getImage();
+    lblImage10.setIcon(new ImageIcon(img10));
+    lblImage10.setBounds(981, 0, 285, 233);
+    contentPane.add(lblImage10);
+    
+    Image img4 = new ImageIcon(this.getClass().getResource("/Pics"
+        + "/left-arrow (1).png")).getImage();
+    btnBackButton.setIcon(new ImageIcon(img4));
+    btnBackButton.setFont(new Font("Tahoma", Font.BOLD, 24));
+    contentPane.add(btnBackButton);
+
+    JLabel lblImage14 = new JLabel("");
+    Image img14 = new ImageIcon(this.getClass().getResource("/Pics"
+        + "/stay-home.png")).getImage();
+    lblImage14.setIcon(new ImageIcon(img14));
+    lblImage14.setBounds(560, 265, 128, 128);
+    contentPane.add(lblImage14);
+  }
 }
