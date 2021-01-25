870cdf75554269cc55f467ee01140101ea5d06d8
dmavroeidis01 <72823402+dmavroeidis01@users.noreply.github.com>
2021-01-25 16:27:58 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	
-	public static void main(String[] args) {
-		//System.out.println(UpdateTheRecords.giveMeTheNameOfStudent(8190001));
-		//UpdateTheRecords.changeTheStatus(8190001, "p", "student");
-		System.out.println(UpdateTheRecords.sendEmailToStaff(730269));
-	}
-}
